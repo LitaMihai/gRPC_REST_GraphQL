eaa35b8bdb9f25e90a000775739c4cef70382d0d
LitaMihai <litamihai13@gmail.com>
2025-03-13 14:40:33 +0200

    Not working (yet) Unversioned files

---
+++ api-comparison/src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../../services/api.service';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [CommonModule],
+  templateUrl: 'dashboard.component.html',
+  styleUrl: 'dashboard.component.css'
+})
+export class DashboardComponent implements OnInit {
+  data: any;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getData().subscribe(response => {
+      this.data = response;
+    });
+  }
+}
